89daf27aa12d6415d24044fdd2332e6801d86694
Saeeda <saeeda123@gmail.com>
2026-01-02 22:34:08 +0500

    update folder

---
+++ script.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1440px',
+            // => @media (min-width: 1440px) { ... }
+          },
+        colors: {
+          'blue' :'#0071e3',
+        }
+      }
+    }
+  }
