4df1a1c8a6b34322e05a0fa8fbf440edd4e2fe3f
Saeeda <saeeda123@gmail.com>
2026-01-04 20:18:23 +0500

    update folder

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-tailwind.config = {
-    theme: {
-      extend: {
-        screens: {
-            'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-      
-            'md': '768px',
-            // => @media (min-width: 768px) { ... }
-      
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-      
-            'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
-      
-            '2xl': '1440px',
-            // => @media (min-width: 1440px) { ... }
-          },
-        colors: {
-          'blue' :'#0071e3',
-        }
-      }
+// scoll button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  // Show / hide button on scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollTopBtn.classList.remove("opacity-0", "invisible");
+      scrollTopBtn.classList.add("opacity-100", "visible");
+    } else {
+      scrollTopBtn.classList.add("opacity-0", "invisible");
+      scrollTopBtn.classList.remove("opacity-100", "visible");
     }
-  }
+  });
+
+  // Scroll smoothly to top
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+// menu Btn
+  const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const mobileLinks = mobileMenu.querySelectorAll("a");
+
+  // Toggle menu on button click
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  // Close menu when clicking any mobile link
+  mobileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
+    });
+  });
